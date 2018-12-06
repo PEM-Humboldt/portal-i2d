3089be7373a31b1b7f75106d284b556992a19130
Erika Suárez Valencia <marvinesv@gmail.com>
2018-12-06 10:46:08 -0500

    Update ceiba ES url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.port = 3000;
-config.bioSearchUrl = 'http://localhost:9200/';
+config.bioSearchUrl = 'http://192.168.11.82:81/es/';
 config.geoSearchUrl = 'http://geonetwork.humboldt.org.co/geonetwork/srv/eng/xml.search';
 config.geoMetadataUrl = 'http://geonetwork.humboldt.org.co/geonetwork/srv/eng/xml.metadata.get';
 config.bioResultSize = 5;
