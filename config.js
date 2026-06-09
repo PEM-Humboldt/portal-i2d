05f58b300663e28475a0246ccb0a9411c608d116
Camilo Zapata <cazm013@gmail.com>
2026-06-09 13:16:16 -0500

    Add S to geonetwork base URLs

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config = {};
 
 config.port = 3000;
 config.bioSearchUrl = 'http://192.168.11.82:81/es/';
-config.geoSearchUrl = 'http://geonetwork.humboldt.org.co/geonetwork/srv/eng/xml.search';
-config.geoMetadataUrl = 'http://geonetwork.humboldt.org.co/geonetwork/srv/eng/xml.metadata.get';
+config.geoSearchUrl = 'https://geonetwork.humboldt.org.co/geonetwork/srv/eng/xml.search';
+config.geoMetadataUrl = 'https://geonetwork.humboldt.org.co/geonetwork/srv/eng/xml.metadata.get';
 config.bioResultSize = 5;
 config.geoResultSize = 5;
 
